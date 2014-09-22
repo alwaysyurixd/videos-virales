6ec7f4dc14f9f22ef2a06fb0df5552c4ded656c1
alwaysyurixd <alwaysyurixd@hotmail.com>
2014-09-21 19:21:16 -0500

    busqueda optimizada3

--- globales.php
+++ globales.php
@@ -1,7 +1,7 @@
 <?php 
 $frase_semana="Y cada noche me pregunto si usted al cerrar los ojos piensa en mi.";
 $servidor="localhost";
-$usuarioBD="adminWKxXR4i ";
+$usuarioBD="adminWKxXR4i";
 $passwordBD="VX8pS9ReZmQ7";
 $base_datos="videos";
  ?>
