d27d0b3e2cfd1a58cade7e41336edb39a0a402b1
alwaysyurixd <alwaysyurixd@hotmail.com>
2014-09-26 21:17:16 -0500

    problemas conexion bd

--- globales.php
+++ globales.php
@@ -1,6 +1,6 @@
 <?php 
 $frase_semana="Y cada noche me pregunto si usted al cerrar los ojos piensa en mi.";
-$servidor="127.9.167.130";
+$servidor="127.9.167.130";dgvj,hikufyjkyuigh
 $usuarioBD="adminWKxXR4i";
 $passwordBD="VX8pS9ReZmQ7";
 $base_datos="videos";
